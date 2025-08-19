205f243be241c251887278780da9d9e90b22398b
Jonathon Kischuk <107976831+jonathonkischuk@users.noreply.github.com>
2025-08-19 02:13:19 -0400

    random walk and black scholes
    Models related to helping reduce risk and predict options pricing and more

---
+++ black_scholes.py
@@ -0,0 +1,40 @@
+from scipy import stats
+from numpy import log, exp, sqrt
+from scipy.constants import sigma
+
+
+def call_option_price(S, E, T, rf, sigma):
+    # first we have to calculate d1 and d2 parameters
+    d1 = (log(S / E) + (rf + sigma * sigma / 2.0) * T) / (sigma * sqrt(T))
+    d2 = d1 - sigma * sqrt(T)
+    print("The d1 and d2 parameters: %s, %s" % (d1, d2))
+    # use the N(x) to calculate the price of the option
+    return S * stats.norm.cdf(d1) - E * exp(-rf * T) * stats.norm.cdf(d2)
+
+
+def put_option_price(S, E, T, rf, sigma):
+    # first we have to calculates s1 and d2 parameters
+    d1 = (log(S / E) + (rf + sigma * sigma / 2.0) * T) / (sigma * sqrt(T))
+    d2 = d1 - sigma * sqrt(T)
+    print("The d1 and d2 parameters: %s, %s" % (d1, d2))
+    # use the N(x) to calculate the price of the option
+    return -S * stats.norm.cdf(-d1) + E * exp(-rf * T) * stats.norm.cdf(-d2)
+
+
+if __name__ == "__main__":
+    # underlying stock price at t = 0
+    S0 = 100
+    # strike price
+    E = 100
+    # expiry 1 year = 365 days
+    T = 1
+    # risk-free rate
+    rf = 0.05
+    # volatility of the underlying stock
+    sigma = 0.2
+
+    print("Call option price accordingg to Black Scholes model: ",
+          call_option_price(S0, E, T, rf, sigma))
+
+    print("put option price accordingg to Black Scholes model: ",
+          put_option_price(S0, E, T, rf, sigma))
