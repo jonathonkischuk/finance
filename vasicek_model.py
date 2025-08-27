a0cb66365a33868511ce2e216b8f2096b7221250
Jonathon Kischuk <107976831+jonathonkischuk@users.noreply.github.com>
2025-08-27 01:44:38 -0400

    Updated with additional models

---
+++ vasicek_model.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def vasicek_model(r0, kappa, theta, sigma, T=1, N=10000):
+    dt = T/float(N)
+    t = np.linspace(0, T, N + 1)
+    rates = [r0]
+
+    for _ in range(N):
+        dr = kappa * (theta - rates[-1]) * dt + sigma * np.sqrt(dt) * np.random.normal()
+        rates.append(rates[-1] + dr)
+
+    return t, rates
+
+
+def plot_model(t, r):
+    plt.plot(t, r)
+    plt.xlabel('Time (t)')
+    plt.ylabel('Interest Rate r(t)')
+    plt.title('Vasicek Model')
+    plt.show()
+
+
+if __name__ == '__main__':
+
+    time, data = vasicek_model(1.3, 0.9, 1.5, 0.01)
+    plot_model(time, data)
